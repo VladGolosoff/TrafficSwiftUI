8c94805d1a452e369cebc816ffd56bfbc8572b07
VladGolosoff <v.golosoff@gmail.com>
2022-01-20 22:27:50 +0300

    Initial Commit

---
+++ TrafficSwiftUI/TrafficSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficSwiftUIApp.swift
+//  TrafficSwiftUI
+//
+//  Created by Владислав Голосов on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
